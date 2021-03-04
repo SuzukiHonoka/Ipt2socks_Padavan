34f499500c28f79a20f73d6e420b133b72702055
铃木和花 <49331402+SuzukiHonoka@users.noreply.github.com>
2021-03-04 14:12:25 +0800

    fix more

--- ipt2socks.sh
+++ ipt2socks.sh
@@ -23,7 +23,7 @@ func_save(){
 }
 
 func_start(){
-if [ -n "`pidof redsocks`" ]
+if [ -n "`pidof ipt2socks`" ]
 then
 	func_stop
 	logger -t $BINARY_NAME "ALREADY RUNNING: KILLED"
@@ -41,8 +41,8 @@ logger -t $BINARY_NAME "STARTED."
 }
 
 func_clean(){
-iptables -t nat -D PREROUTING `iptables -t nat -L PREROUTING --line-numbers|grep $CHAIN_NAME|head -n 1|tr -cd "[0-9]"`
-iptables -t nat -D OUTPUT `iptables -t nat -L OUTPUT --line-numbers|grep $CHAIN_NAME|head -n 1|tr -cd "[0-9]"`
+iptables -t nat -D PREROUTING `iptables -t nat -L PREROUTING --line-numbers|grep $CHAIN_NAME|head -n 1|awk '{print $1}'`
+iptables -t nat -D OUTPUT `iptables -t nat -L OUTPUT --line-numbers|grep $CHAIN_NAME|head -n 1|awk '{print $1}'`
 iptables -t nat -F $CHAIN_NAME
 iptables -t nat -X $CHAIN_NAME
 }
